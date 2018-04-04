d3c5ee02a8771db1738d3f78301435ed23cd838d
eddicke <adenekanademola931@gmail.com>
2018-04-04 19:00:27 -0400

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,9 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
    io.emit('chat message', msg);
   });
+  socket.on('onlinecount', function(msg){
+   io.emit('onlinecount', msg);
+  });
 });
 
 http.listen(port, function(){
