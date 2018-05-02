5b0c8fd3c578290d6cca24a1534ffa6684570639
eddicke <adenekanademola931@gmail.com>
2018-05-02 18:35:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -36,10 +36,11 @@ io.on('connection', function(socket) {
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   })
+   io.sockets.emit('state', players);
 });
 
 
-  io.sockets.emit('state', players);
+ 
 
 
 
