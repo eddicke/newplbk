b15090a11f8097ed7e48def55fc64a0996f895b2
eddicke <adenekanademola931@gmail.com>
2018-05-03 17:16:11 -0400

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ setInterval(function(){
   if(count == 1){
 
   io.sockets.emit('state', players);
-    io.sockets.emit('directions', dirs);
+    
   }
 }, 1000)
 
@@ -77,7 +77,7 @@ setInterval(function(){
 //}, 1000 / 60);
 
 setInterval(function() {
-   
+   io.sockets.emit('directions', dirs.rnd);
     io.sockets.emit('online', mrt.rnd);
   
 }, 1000 / 60);
