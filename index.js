766aff420547bbca2b6a3dcb0581dde0032ed8cf
eddicke <adenekanademola931@gmail.com>
2018-05-03 17:26:43 -0400

    Update index.js

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ io.on('connection', function(socket) {
     }
     
     players[socket.id] = {
-      x: dirs[socket.id].x,
-      y: dirs[socket.id].y,
+      x: 0,
+      y: 0,
       rnd: Math.floor(Math.random()*10),
       mesh: null,
       color: 'rgb(' + parseInt(Math.random() * 255) + ',' + parseInt(Math.random() * 255) + ',' + parseInt(Math.random() * 255) + ')'
