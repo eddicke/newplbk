16b297c903708bca41ec3568bf48076a438ff879
eddicke <adenekanademola931@gmail.com>
2018-05-04 00:24:22 -0400

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ io.on('connection', function(socket) {
        dirs[socket.id] = {
       x: 0,
       y: 0,
-         rnd: 2,
+         rnd: Math.floor(Math.random()*10),
          move: false
     }
     
